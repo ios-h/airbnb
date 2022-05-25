ebbc8fc0bb1897f3e3bef6591643c39f7a9adcc8
sanghee <anshee97@naver.com>
2022-05-26 01:39:19 +0900

    chore: UIColor Extension non-optional하게 사용할 수 있도록 수정 (ios-h/airbnb#9)

--- iOS/airbnb/Common/Extension/UIColor+Extension.swift
+++ iOS/airbnb/Common/Extension/UIColor+Extension.swift
@@ -8,15 +8,58 @@
 import UIKit
 
 extension UIColor {
-    static var black: UIColor? { return UIColor(named: "black") }
-    static var blue: UIColor? { return UIColor(named: "blue") }
-    static var gray1: UIColor? { return UIColor(named: "gray1") }
-    static var gray2: UIColor? { return UIColor(named: "gray2") }
-    static var gray3: UIColor? { return UIColor(named: "gray3") }
-    static var gray4: UIColor? { return UIColor(named: "gray4") }
-    static var gray5: UIColor? { return UIColor(named: "gray5") }
-    static var gray6: UIColor? { return UIColor(named: "gray6") }
-    static var primary: UIColor? { return UIColor(named: "primary") }
-    static var secondary: UIColor? { return UIColor(named: "secondary") }
-    static var white: UIColor? { return UIColor(named: "white") }
+    static var customBlack: UIColor {
+        guard let color = UIColor(named: "black") else { return UIColor.black }
+        return color
+    }
+    
+    static var blue: UIColor {
+        guard let color = UIColor(named: "blue") else { return UIColor.systemBlue }
+        return color
+    }
+    
+    static var gray1: UIColor {
+        guard let color = UIColor(named: "gray1") else { return UIColor.systemGray }
+        return color
+    }
+    
+    static var gray2: UIColor {
+        guard let color = UIColor(named: "gray2") else { return UIColor.systemGray2 }
+        return color
+    }
+    
+    static var gray3: UIColor {
+        guard let color = UIColor(named: "gray3") else { return UIColor.systemGray3 }
+        return color
+    }
+    
+    static var gray4: UIColor {
+        guard let color = UIColor(named: "gray4") else { return UIColor.systemGray4 }
+        return color
+    }
+    
+    static var gray5: UIColor {
+        guard let color = UIColor(named: "gray5") else { return UIColor.systemGray5 }
+        return color
+    }
+    
+    static var gray6: UIColor {
+        guard let color = UIColor(named: "gray6") else { return UIColor.systemGray6 }
+        return color
+    }
+    
+    static var primary: UIColor {
+        guard let color = UIColor(named: "primary") else { return UIColor.red }
+        return color
+    }
+    
+    static var secondary: UIColor {
+        guard let color = UIColor(named: "secondary") else { return UIColor.green }
+        return color
+    }
+    
+    static var white: UIColor {
+        guard let color = UIColor(named: "white") else { return UIColor.white }
+        return color
+    }
 }
