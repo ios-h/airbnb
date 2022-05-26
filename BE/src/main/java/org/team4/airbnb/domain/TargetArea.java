95a7b9a29a44cfacd1dd5addc30c2c6844589e25
Seung-Wan Jeong <sjeong4776@gmail.com>
2022-05-26 12:26:38 +0900

    feat: TargetArea 엔티티 추가 (ios-h/airbnb#3)
    - 위치 조건 입력 시 주소 자동완성 및 검색 기준으로 쓰일 좌표값을 제공하는 역할.

---
+++ BE/src/main/java/org/team4/airbnb/domain/TargetArea.java
@@ -0,0 +1,19 @@
+package org.team4.airbnb.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class TargetArea {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "target_area_id")
+    private Long id;
+
+    private Address address;
+
+    private Geolocation geolocation;
+}
