58c49afdad31c8be072eda39eaed853d0e045101
mybloom <baejungeun11@gmail.com>
2022-05-28 01:57:31 +0900

    feat: 숙소 데이터 생성을 위한 외부 API call (ios-h/airbnb#10)
    - uri를 /dummydata 로 RestController로 작성

---
+++ BE/src/main/java/org/team4/airbnb/dummydata/ApiCallParse.java
@@ -0,0 +1,60 @@
+package org.team4.airbnb.dummydata;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URLEncoder;
+
+import org.apache.tomcat.util.json.ParseException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@PropertySource(value = "classpath:api.properties", ignoreResourceNotFound = true)
+public class ApiCallParse {
+
+	@Value("${accommodation.api.serviceKey}")
+	private String serviceKey;
+
+	@GetMapping("/dummydata")
+	public String getDummyData() throws ParseException, UnsupportedEncodingException {
+		String url = prepareRequestUrl();
+		ResponseEntity<String> responseEntity = callApi(url);
+		String response = responseEntity.toString();
+
+		return response;
+	}
+
+	private String prepareRequestUrl() throws UnsupportedEncodingException {
+		String url = "";
+
+		StringBuilder urlBuilder = new StringBuilder("https://openapi.gg.go.kr/StayingGeneralHotel"); /*URL*/
+		urlBuilder.append("?" + URLEncoder.encode("KEY", "UTF-8") + "=" + serviceKey); /*Service Key*/
+		urlBuilder.append("&" + URLEncoder.encode("Type", "UTF-8") + "=" + URLEncoder.encode( "json","UTF-8")); /*호출 문서(xml, json)*/
+		urlBuilder.append("&" + URLEncoder.encode("pSize", "UTF-8") + "=" + URLEncoder.encode("20","UTF-8")); /*페이지 크기(기본100)*/
+		urlBuilder.append("&" + URLEncoder.encode("pIndex", "UTF-8") + "=" + URLEncoder.encode("1","UTF-8")); /*시작 페이지(기본1)*/
+
+		url = urlBuilder.toString();
+		return url;
+	}
+
+	private ResponseEntity<String> callApi(String url) {
+		RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+
+		HttpEntity<HttpHeaders> entity = new HttpEntity<>(headers);
+
+		ResponseEntity<String> response = restTemplate.exchange(URI.create(url), HttpMethod.GET,
+			entity, String.class);
+
+		return response;
+	}
+}
