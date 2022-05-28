cbad06d7779f99d0cef60401aee5fd0634c79fa3
mybloom <baejungeun11@gmail.com>
2022-05-28 15:29:25 +0900

    feat: bulk insert repository용 도메인 클래스 생성 (ios-h/airbnb#10)

---
+++ BE/src/main/java/org/team4/airbnb/dummydata/AccommodationDB.java
@@ -0,0 +1,17 @@
+package org.team4.airbnb.dummydata;
+
+import org.team4.airbnb.domain.AccommodationType;
+
+public class AccommodationDB {
+
+	private String name;
+	private double latitude;
+	private double longitude;
+	private String address;
+	private String type;
+	private int numberOfRoom;
+	private int numberOfBed;
+	private int numberOfBathRoom;
+	private int price;
+	private int max_guest;
+}
