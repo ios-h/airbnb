25c96797b20d048871a99339eff26c6c2badb2de
sanghee <anshee97@naver.com>
2022-05-30 17:18:22 +0900

    chore: HeroImageSection에 들어갈 MockDataModel 추가 (ios-h/airbnb#16)

---
+++ iOS/airbnb/Common/MockDataModel.swift
@@ -0,0 +1,17 @@
+//
+//  MockDataModel.swift
+//  airbnb
+//
+//  Created by 안상희 on 2022/05/30.
+//
+
+import Foundation
+
+class MockDataModel {
+    static let mockHeroImages = [HeroImageItem(title: "슬기로운 자연생활", imageName: "img_hero"),
+                          HeroImageItem(title: "바다로 떠나요", imageName: "img_hero_beach"),
+                          HeroImageItem(title: "우리의 제주", imageName: "img_hero_jeju"),
+                          HeroImageItem(title: "슬기로운 숲 속 생활", imageName: "img_hero"),
+                          HeroImageItem(title: "바다 어때요?", imageName: "img_hero_beach"),
+                          HeroImageItem(title: "제주로 떠나요", imageName: "img_hero_jeju")]
+}
