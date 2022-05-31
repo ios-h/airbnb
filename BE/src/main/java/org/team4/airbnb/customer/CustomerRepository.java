4e804349d39a8a4f6d19e5d9cf7437794245cc0f
mybloom <baejungeun11@gmail.com>
2022-05-31 10:18:19 +0900

    feat: CustomerRepository interface 생성 (ios-h/airbnb#14)

---
+++ BE/src/main/java/org/team4/airbnb/customer/CustomerRepository.java
@@ -0,0 +1,7 @@
+package org.team4.airbnb.customer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+}
