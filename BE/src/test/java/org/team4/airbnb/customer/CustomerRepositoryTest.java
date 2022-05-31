340022494f010a8a11acc5e1591953cdd06e1d4f
mybloom <baejungeun11@gmail.com>
2022-05-31 11:37:11 +0900

    test: customerId로 위시리스트 조회하는 테스트코드 작성(ios-h/airbnb#14)

---
+++ BE/src/test/java/org/team4/airbnb/customer/CustomerRepositoryTest.java
@@ -0,0 +1,31 @@
+package org.team4.airbnb.customer;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+class CustomerRepositoryTest {
+
+	@Autowired
+	CustomerRepository customerRepository;
+
+	@Test
+	@DisplayName("customerId로 위시리스트 조회하기")
+	void findById() {
+		//when
+		Optional<Customer> findCustomer = customerRepository.findById(1L);
+		List<Wish> wishes = findCustomer.get().getWishes();
+
+		//then
+		assertThat(findCustomer).isNotEmpty();
+		assertThat(wishes).hasSize(3).anyMatch(wish -> wish.getCustomer().getId() == 1);
+	}
+}
