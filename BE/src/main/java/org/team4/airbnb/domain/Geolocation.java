3dd291e7279c00ce9fb6ef08e3484b5b513b2471
Seung-Wan Jeong <sjeong4776@gmail.com>
2022-05-31 18:29:47 +0900

    chore: 현 시점에서 사용하지 않는 애너테이션 제거 (ios-h/airbnb#12)

--- BE/src/main/java/org/team4/airbnb/domain/Geolocation.java
+++ BE/src/main/java/org/team4/airbnb/domain/Geolocation.java
@@ -1,14 +1,8 @@
 package org.team4.airbnb.domain;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-
 import javax.persistence.Embeddable;
 
 @Embeddable
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
 public class Geolocation {
 
 	private double longitude;
