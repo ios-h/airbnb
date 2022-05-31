7c1b2b3ac90c399b25edcb5a0d4feea2ca472220
mybloom <baejungeun11@gmail.com>
2022-05-31 12:34:29 +0900

    feat: 위시리스트 저장 구현을 위해 wish 패키지 생성 및 repository 작성 (ios-h/airbnb#14)

--- BE/src/main/java/org/team4/airbnb/customer/Customer.java
+++ BE/src/main/java/org/team4/airbnb/customer/Customer.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+import org.team4.airbnb.wish.Wish;
 
 @Entity
 @NoArgsConstructor
