55b7fb0ef13331e52289a06292ab14bceb549be4
mybloom <baejungeun11@gmail.com>
2022-06-02 10:17:28 +0900

    refactor: AccommodationType enum에 속성, 메서드 추가 (ios-h/airbnb#23)

--- BE/src/main/java/org/team4/airbnb/accommodation/AccommodationType.java
+++ BE/src/main/java/org/team4/airbnb/accommodation/AccommodationType.java
@@ -1,5 +1,34 @@
 package org.team4.airbnb.accommodation;
 
+import java.util.Arrays;
+import org.team4.airbnb.exception.CustomException;
+import org.team4.airbnb.exception.ErrorCode;
+
 public enum AccommodationType {
-	HOTEL
+	HOTEL("일반 호텔");
+
+	private String dbData;
+
+	AccommodationType(String dbData) {
+		this.dbData = dbData;
+	}
+
+	public static AccommodationType ofDbData(String dbData) {
+		return Arrays.stream(AccommodationType.values())
+			.filter(accommodationType -> accommodationType.getDbData().equals(dbData))
+			.findAny()
+			.orElseThrow(() -> new CustomException(ErrorCode.NO_DATA_FOUND_ACCOMMODATION_TYPE));
+	}
+
+	public String getDbData() {
+		return dbData;
+	}
+
+	public static void main(String[] args) {
+		AccommodationType type = AccommodationType.ofDbData("일반 호텔");
+		AccommodationType hotel = AccommodationType.HOTEL;
+		System.out.println("**type=" + type);
+		System.out.println("**hotel=" + hotel);
+		System.out.println("type.eq.hotel=" + type.equals(hotel));
+	}
 }
