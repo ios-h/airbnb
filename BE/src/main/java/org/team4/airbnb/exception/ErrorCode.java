dc4ad0a680308b4d8deba17730dac3cb0f419e94
mybloom <baejungeun11@gmail.com>
2022-06-02 10:30:09 +0900

    fix: 키보드 잘못눌러서 오타 들어갔음  (ios-h/airbnb#23)

--- BE/src/main/java/org/team4/airbnb/exception/ErrorCode.java
+++ BE/src/main/java/org/team4/airbnb/exception/ErrorCode.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
 	NO_DATA_FOUND_CUSTOMER(HttpStatus.NOT_FOUND, "존재하지 않은 회원입니다."),
-	ㅊ(HttpStatus.NOT_FOUND, "존재하지 숙소 타입입니다.");
+	NO_DATA_FOUND_ACCOMMODATION_TYPE(HttpStatus.NOT_FOUND, "존재하지 숙소 타입입니다.");
 
 	final HttpStatus status;
 	final String message;
