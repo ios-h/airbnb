2f87aa7c6428704f0822c0898e1a9d132efb3c1d
mybloom <baejungeun11@gmail.com>
2022-06-04 07:25:37 +0900

    refactor: Customer 엔티티에 toString() 추가 (ios-h/airbnb#23)

--- BE/src/main/java/org/team4/airbnb/customer/Customer.java
+++ BE/src/main/java/org/team4/airbnb/customer/Customer.java
@@ -1,8 +1,14 @@
 package org.team4.airbnb.customer;
 
-import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.team4.airbnb.wish.Wish;
@@ -21,4 +27,12 @@ public class Customer {
 
 	@OneToMany(mappedBy = "customer", fetch = FetchType.LAZY)
 	List<Wish> wishes = new ArrayList<>();
+
+	@Override
+	public String toString() {
+		return "Customer{" +
+			"id=" + id +
+			", userId='" + userId + '\'' +
+			'}';
+	}
 }
