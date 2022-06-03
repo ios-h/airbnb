7ab1b24b9ff1307616d51467a6fcdbe82ca4ca92
mybloom <baejungeun11@gmail.com>
2022-06-04 08:32:21 +0900

    feat: 테스트코드 엔티티 생성 유틸 클래스에 Wish3개 생성하는 메서드 추가  (ios-h/airbnb#23)

--- BE/src/test/java/org/team4/airbnb/util/EntityCreator.java
+++ BE/src/test/java/org/team4/airbnb/util/EntityCreator.java
@@ -1,5 +1,7 @@
 package org.team4.airbnb.util;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.team4.airbnb.accommodation.Accommodation;
 import org.team4.airbnb.accommodation.AccommodationRepository;
 import org.team4.airbnb.customer.Customer;
@@ -18,5 +20,22 @@ public class EntityCreator {
 		wish.setAccommodationId(accommodation.getId());
 
 		return wish;
+	}
+
+	public static List<Wish> create3Wishes(CustomerRepository customerRepository,
+		AccommodationRepository accommodationRepository) {
+		Customer customer = customerRepository.findFirstBy();
+		List<Accommodation> accommodations = accommodationRepository.findTop3ByOrderById();
+
+		List<Wish> wishes = new ArrayList<>();
+		for (int i = 0; i < 3; i++) {
+			Wish wish = new Wish();
+			wish.setCustomer(customer);
+			wish.setAccommodationId(accommodations.get(i).getId());
+
+			wishes.add(wish);
+		}
+
+		return wishes;
 	}
 }
