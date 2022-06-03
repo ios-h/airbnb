7996fba486e4a49e3f77b3ce3e2b983e2e05ee8e
mybloom <baejungeun11@gmail.com>
2022-06-04 07:29:21 +0900

    refactor:위시리스트 추가하는 테스트시 Wish 객체를 생성할 때 필요한 의존 데이터를 db에서 불러오도록 설정 (ios-h/airbnb#23)
    - 의존 데이터인 Customer, Accommodation을 db에서 가져와서 생성

--- BE/src/test/java/org/team4/airbnb/util/EntityCreator.java
+++ BE/src/test/java/org/team4/airbnb/util/EntityCreator.java
@@ -1,15 +1,21 @@
 package org.team4.airbnb.util;
 
+import org.team4.airbnb.accommodation.Accommodation;
+import org.team4.airbnb.accommodation.AccommodationRepository;
+import org.team4.airbnb.customer.Customer;
 import org.team4.airbnb.customer.CustomerRepository;
 import org.team4.airbnb.wish.Wish;
 
 public class EntityCreator {
 
-	public static Wish createWish(CustomerRepository customerRepository, Long customerId,
-		Long accommodationId) {
+	public static Wish createWish(CustomerRepository customerRepository,
+		AccommodationRepository accommodationRepository) {
+		Customer customer = customerRepository.findFirstBy();
+		Accommodation accommodation = accommodationRepository.findFirstBy();
+
 		Wish wish = new Wish();
-		wish.setCustomer(customerRepository.findById(customerId).get());
-		wish.setAccommodationId(accommodationId);
+		wish.setCustomer(customer);
+		wish.setAccommodationId(accommodation.getId());
 
 		return wish;
 	}
