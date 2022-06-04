67a86422cce9428c9aa00a7cfcce8a356b4dc79f
mybloom <baejungeun11@gmail.com>
2022-06-04 09:55:28 +0900

    feat: 위시리스트에 복합 unique 제약사항 추가 - customer_id, accommodation_id (ios-h/airbnb#23)

--- BE/docs/airbnb_table_create.sql
+++ BE/docs/airbnb_table_create.sql
@@ -146,3 +146,5 @@ CREATE TABLE IF NOT EXISTS `wish`
     ENGINE = InnoDB
     DEFAULT CHARACTER SET = utf8mb4
     COLLATE = utf8mb4_0900_ai_ci;
+
+ALTER TABLE wish ADD UNIQUE (customer_id, accommodation_id);
