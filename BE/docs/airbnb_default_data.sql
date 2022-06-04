6306cc7d4492f6c5ab0f0cf9b43eb05265818514
mybloom <baejungeun11@gmail.com>
2022-06-04 22:42:02 +0900

    data: 숙소 이미지 샘플 데이터 입력 쿼리  (ios-h/airbnb#19)

--- BE/docs/airbnb_default_data.sql
+++ BE/docs/airbnb_default_data.sql
@@ -22,3 +22,33 @@ VALUES
     ('2022-04-20', '2022-04-25', 1, 0, 234, 2345, 23, 24, 25, 26, 11, 1),
     ('2022-05-11', '2022-05-13', 4, 2, 345, 34556, 34, 34, 34, 35, 16, 2),
     ('2022-06-13', '2022-06-22', 3, 1, 456, 45555, 45, 43, 42, 44, 33, 2);
+
+
+-- -----------------------------------------------------
+-- accommodation_image
+-- -----------------------------------------------------
+
+insert into accommodation_image(url,image_seq,accommodation_id) values('https://imgur.com/rtEuIAh',1,1);
+insert into accommodation_image(url,image_seq,accommodation_id) values('https://imgur.com/h6Ok832',2,1);
+insert into accommodation_image(url,image_seq,accommodation_id) values('https://imgur.com/GUWrNUJ',3,1);
+insert into accommodation_image(url,image_seq,accommodation_id) values('https://imgur.com/a/9CZ7Z9L',4,1);
+insert into accommodation_image(url,image_seq,accommodation_id) values('https://imgur.com/rtEuIAh',1,2);
+insert into accommodation_image(url,image_seq,accommodation_id) values('https://imgur.com/h6Ok833',2,2);
+insert into accommodation_image(url,image_seq,accommodation_id) values('https://imgur.com/GUWrNUJ',3,2);
+insert into accommodation_image(url,image_seq,accommodation_id) values('https://imgur.com/a/9CZ7Z10L',4,2);
+insert into accommodation_image(url,image_seq,accommodation_id) values('https://imgur.com/rtEuIAh',1,3);
+insert into accommodation_image(url,image_seq,accommodation_id) values('https://imgur.com/h6Ok834',2,3);
+insert into accommodation_image(url,image_seq,accommodation_id) values('https://imgur.com/GUWrNUJ',3,3);
+insert into accommodation_image(url,image_seq,accommodation_id) values('https://imgur.com/a/9CZ7Z11L',4,3);
+insert into accommodation_image(url,image_seq,accommodation_id) values('https://imgur.com/rtEuIAh',1,4);
+insert into accommodation_image(url,image_seq,accommodation_id) values('https://imgur.com/h6Ok835',2,4);
+insert into accommodation_image(url,image_seq,accommodation_id) values('https://imgur.com/GUWrNUJ',3,4);
+insert into accommodation_image(url,image_seq,accommodation_id) values('https://imgur.com/a/9CZ7Z9L',4,4);
+insert into accommodation_image(url,image_seq,accommodation_id) values('https://imgur.com/rtEuIAh',1,5);
+insert into accommodation_image(url,image_seq,accommodation_id) values('https://imgur.com/h6Ok836',2,5);
+insert into accommodation_image(url,image_seq,accommodation_id) values('https://imgur.com/GUWrNUJ',3,5);
+insert into accommodation_image(url,image_seq,accommodation_id) values('https://imgur.com/a/9CZ7Z10L',4,5);
+insert into accommodation_image(url,image_seq,accommodation_id) values('https://imgur.com/rtEuIAh',1,6);
+insert into accommodation_image(url,image_seq,accommodation_id) values('https://imgur.com/h6Ok837',2,6);
+insert into accommodation_image(url,image_seq,accommodation_id) values('https://imgur.com/GUWrNUJ',3,6);
+insert into accommodation_image(url,image_seq,accommodation_id) values('https://imgur.com/a/9CZ7Z11L',4,6);
