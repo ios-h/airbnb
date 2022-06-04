f31e32c40cdea7ce242ae1a2946b7d2fa15a296d
mybloom <baejungeun11@gmail.com>
2022-06-04 17:17:13 +0900

    refactor: Accommodation의 데이터타입을 wrapper Class로 변경(ios-h/airbnb#19)
    - null 값을 가진 경우와 정상적인 경로로 숫자 0이 입력된 경우를 구분하기 위해서 wrapper class로 일괄 변경

--- BE/src/main/java/org/team4/airbnb/accommodation/RoomFeature.java
+++ BE/src/main/java/org/team4/airbnb/accommodation/RoomFeature.java
@@ -5,8 +5,8 @@ import javax.persistence.Embeddable;
 @Embeddable
 public class RoomFeature {
 
-	private int maxGuest;
-	private int numberOfRoom;
-	private int numberOfBed;
-	private int numberOfBathroom;
+	private Integer maxGuest;
+	private Integer numberOfRoom;
+	private Integer numberOfBed;
+	private Integer numberOfBathroom;
 }
