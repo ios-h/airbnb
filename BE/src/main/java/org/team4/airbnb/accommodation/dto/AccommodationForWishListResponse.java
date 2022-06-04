4d2b8b75e95f8b0474cb50a62ad75e99918b4f8a
mybloom <baejungeun11@gmail.com>
2022-06-04 17:19:30 +0900

    feat: 위시리스트 조회 응답을 위한 dto생성  (ios-h/airbnb#19)
    - 클래스명 : AccommodationForWishListResponse

---
+++ BE/src/main/java/org/team4/airbnb/accommodation/dto/AccommodationForWishListResponse.java
@@ -0,0 +1,49 @@
+package org.team4.airbnb.accommodation.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.team4.airbnb.accommodation.Accommodation;
+import org.team4.airbnb.accommodation.AccommodationImage;
+
+@RequiredArgsConstructor
+@Getter
+public class AccommodationForWishListResponse {
+
+	private final List<Element> data;
+
+	private static class Element {
+		private final Long accommodationId;
+		private final String name;
+		private final Integer price;
+		private final List<String> accommodationImages;
+		//		private final Review review;
+		private final Long wishId;
+		private final Boolean isWish;
+
+		Element(Accommodation accommodation, Long wishId) {
+			this.accommodationId = accommodation.getId();
+			this.name = accommodation.getName();
+			this.price = accommodation.getPrice();
+			this.accommodationImages = accommodation.getAccommodationImages()
+				.stream()
+				.map(AccommodationImage::getUrl)
+				.collect(Collectors.toList());
+			this.wishId = wishId;
+			this.isWish = true;
+		}
+	}
+
+	public static AccommodationForWishListResponse from(List<Accommodation> accommodations,
+		List<Long> wishIds) {
+		List<Element> listForWish = new ArrayList<>();
+		for (int i = 0; i < wishIds.size(); i++) {
+			Element element = new Element(accommodations.get(i), wishIds.get(i));
+			listForWish.add(element);
+		}
+		return new AccommodationForWishListResponse(listForWish);
+	}
+
+}
