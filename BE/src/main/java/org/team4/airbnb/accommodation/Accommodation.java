7e41678888588ecf086500f3f926a77987caf342
mybloom <baejungeun11@gmail.com>
2022-06-06 13:52:31 +0900

    refactor: 엔티티 -> DTO 변환을 엔티티에서 해보기 (ios-h/airbnb#31)
    - 아닌 것 같아서 원복할 예정

--- BE/src/main/java/org/team4/airbnb/accommodation/Accommodation.java
+++ BE/src/main/java/org/team4/airbnb/accommodation/Accommodation.java
@@ -15,6 +15,7 @@ import javax.persistence.OrderBy;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.team4.airbnb.customer.dto.AccommodationForWishListResponse2;
 import org.team4.airbnb.domain.BaseCreated;
 import org.team4.airbnb.domain.Geolocation;
 
@@ -52,6 +53,22 @@ public class Accommodation extends BaseCreated {
 	@OneToMany(mappedBy = "accommodation", fetch = FetchType.LAZY)
 	@OrderBy("imageSeq asc")
 	private List<AccommodationImage> accommodationImages = new ArrayList<>();
+
+	private List<String> askImagesUrls() {
+		return accommodationImages.get(0).offerImagesUrls(accommodationImages);
+	}
+
+	//엔티티 to DTO 를 엔티티에서 수행 - 아닌 것 같아서 원복 예정
+	public static AccommodationForWishListResponse2 toWishListDto(Accommodation accommodation,
+		Long wishId) {
+		return AccommodationForWishListResponse2.builder()
+			.accommodationId(accommodation.id)
+			.name(accommodation.name)
+			.price(accommodation.price)
+			.imagesUrls(accommodation.askImagesUrls())
+			.wishId(wishId)
+			.build();
+	}
 
 	public boolean isNotNullName(Accommodation accommodation) {
 		return !accommodation.name.isEmpty();
