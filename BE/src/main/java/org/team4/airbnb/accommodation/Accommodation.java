7334a83f456971cba0ebe6ae034e9287b4e00f05
mybloom <baejungeun11@gmail.com>
2022-06-06 15:17:40 +0900

    refactor: Accommmodation의 images들의 url 리스트를 갖고오기 위한 메서드 생성 (ios-h/airbnb#31)
    - 아닌 것 같아서 원복할 예정

--- BE/src/main/java/org/team4/airbnb/accommodation/Accommodation.java
+++ BE/src/main/java/org/team4/airbnb/accommodation/Accommodation.java
@@ -15,7 +15,6 @@ import javax.persistence.OrderBy;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import org.team4.airbnb.customer.dto.AccommodationForWishListResponse2;
 import org.team4.airbnb.domain.BaseCreated;
 import org.team4.airbnb.domain.Geolocation;
 
@@ -54,20 +53,8 @@ public class Accommodation extends BaseCreated {
 	@OrderBy("imageSeq asc")
 	private List<AccommodationImage> accommodationImages = new ArrayList<>();
 
-	private List<String> askImagesUrls() {
+	public List<String> askImagesUrls() {
 		return accommodationImages.get(0).offerImagesUrls(accommodationImages);
-	}
-
-	//엔티티 to DTO 를 엔티티에서 수행 - 아닌 것 같아서 원복 예정
-	public static AccommodationForWishListResponse2 toWishListDto(Accommodation accommodation,
-		Long wishId) {
-		return AccommodationForWishListResponse2.builder()
-			.accommodationId(accommodation.id)
-			.name(accommodation.name)
-			.price(accommodation.price)
-			.imagesUrls(accommodation.askImagesUrls())
-			.wishId(wishId)
-			.build();
 	}
 
 	public boolean isNotNullName(Accommodation accommodation) {
