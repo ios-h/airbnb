e5048a225f4bea39ae9da228eb58481b12caf1c3
mybloom <baejungeun11@gmail.com>
2022-06-09 21:24:13 +0900

    chore: LoginResponse DTO 객체 패키지 이동으로 인한 import문 수정 (ios-h/airbnb#35)

--- BE/src/main/java/org/team4/airbnb/auth/OAuthController.java
+++ BE/src/main/java/org/team4/airbnb/auth/OAuthController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.team4.airbnb.auth.dto.LoginResponse;
 
 @RequiredArgsConstructor
 @RestController
