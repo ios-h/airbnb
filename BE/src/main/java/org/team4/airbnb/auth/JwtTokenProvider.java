7f25ff75fa7693d777eaf553cff7651696146f2e
mybloom <baejungeun11@gmail.com>
2022-06-10 08:10:47 +0900

    feat: 토큰 파싱하는 메서드 추가, secretKey 인코딩해서 사용 (ios-h/airbnb#35)

--- BE/src/main/java/org/team4/airbnb/auth/JwtTokenProvider.java
+++ BE/src/main/java/org/team4/airbnb/auth/JwtTokenProvider.java
@@ -1,11 +1,13 @@
 package org.team4.airbnb.auth;
 
+
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Header;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
+import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -13,17 +15,18 @@ import java.util.Random;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
+import org.team4.airbnb.exception.TokenInValidateException;
 
 @PropertySource(value = "classpath:jwt.properties", ignoreResourceNotFound = true)
 @Component
-class JwtTokenProvider {
+public class JwtTokenProvider {
 
 	@Value("${jwt.secretKey}")
 	private String secretKey;
 
 	private final long tokenValidityInMilliseconds = Duration.ofMillis(30).toMillis();
 
-	String createAccessToken(String userId){
+	public String createAccessToken(String userId){
 		//private claims 생성
 		Map<String, Object> claimsAttribute = new HashMap<>();
 		claimsAttribute.put("userId",userId);
@@ -32,7 +35,7 @@ class JwtTokenProvider {
 		return createToken(claims);
 	}
 
-	String createRefreshToken(){
+	public String createRefreshToken(){
 		byte[] bytes = new byte[7];
 		new Random().nextBytes(bytes);
 		String claimSubjectForRefreshToken = new String(bytes, StandardCharsets.UTF_8);
@@ -43,8 +46,10 @@ class JwtTokenProvider {
 	}
 
 	private String createToken(Claims claims) {
+		String encodeSecretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
 		long currentTime = System.currentTimeMillis();
 		Date now = new Date();
+
 
 		String jwtToken = Jwts.builder()
 			.setHeaderParam(Header.TYPE, Header.JWT_TYPE) //Header 셋팅 : 토큰 타입 정보 typ
@@ -54,10 +59,34 @@ class JwtTokenProvider {
 			.setExpiration(new Date(now.getTime() + tokenValidityInMilliseconds)) //exp
 			//payload - private claim
 			.setClaims(claims)
-			.signWith(SignatureAlgorithm.HS256, "keySecret") //해싱알고리즘, 시크릿키
-
+			.signWith(SignatureAlgorithm.HS256, encodeSecretKey) //해싱알고리즘, 시크릿키
 			.compact();
 
 		return jwtToken;
+	}
+
+	public Claims parseJwtToken(String header) {
+		String encodeSecretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+
+		validateHeader(header);
+		String token = extractToken(header);
+
+		Claims claims = Jwts.parser()
+			.setSigningKey(encodeSecretKey)
+			.parseClaimsJws(token)
+			.getBody();
+
+		System.out.println();
+		return claims;
+	}
+
+	private void validateHeader(String header) {
+		if (header == null || !header.startsWith("Bearer ")) {
+			throw new TokenInValidateException();
+		}
+	}
+
+	private String extractToken(String authorizationHeader) {
+		return authorizationHeader.substring("Bearer ".length());
 	}
 }
