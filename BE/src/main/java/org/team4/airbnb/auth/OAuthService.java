6b4fb050a59ca18bad1011de3b4e1c4486b4b3e3
mybloom <baejungeun11@gmail.com>
2022-06-09 21:24:48 +0900

    feat: JWT access token 생성하는 로직 추가 (ios-h/airbnb#35)

--- BE/src/main/java/org/team4/airbnb/auth/OAuthService.java
+++ BE/src/main/java/org/team4/airbnb/auth/OAuthService.java
@@ -1,7 +1,12 @@
 package org.team4.airbnb.auth;
 
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 import java.util.Collections;
+import java.util.Date;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.ParameterizedTypeReference;
@@ -12,6 +17,7 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.team4.airbnb.auth.config.MemoryProviderRepository;
 import org.team4.airbnb.auth.config.OauthProvider;
+import org.team4.airbnb.auth.dto.LoginResponse;
 import org.team4.airbnb.auth.dto.OauthTokenResponse;
 import org.team4.airbnb.auth.dto.UserProfile;
 import org.team4.airbnb.customer.Customer;
@@ -34,22 +40,44 @@ public class OAuthService {
 		//2. 유저 정보 얻어오기
 		UserProfile userProfile = getUserInfoFromOauth(provider, oauthProvider,
 			tokenResponse);
+		String userIdViaUserProfile = userProfile.getUserId();
 
-		if (userProfile.getUserId() == null) {
+		if (userIdViaUserProfile == null) {
 			new GithubInfoNotFoundException();
 		}
 
 		//3. DB에 유저 정보 저장 (최초 로그인 시 1번)
-		Customer customer = Customer.of(userProfile.getUserId());
+		Customer customer = Customer.of(userIdViaUserProfile);
 		//기존 db에 사용자 있는지 확인 후, 없다면 DB에 유저 정보 저장 후 로그인처리
-		Customer findCustomer = customerRepository.findByUserId()
+		Customer findCustomer = customerRepository.findByUserId(userIdViaUserProfile)
 			.orElse(null);
 
 		if(findCustomer == null) {
 			customer = customerRepository.save(customer);
 		}
 
+		//4. JWT 토큰 생성 후 전달
+		String jwtToken = makeJwtToken(userIdViaUserProfile);
+
 		return null;
+	}
+
+	private String makeJwtToken(String userId){
+		long currentTime = System.currentTimeMillis();
+		Date now = new Date();
+
+		String jwtToken = Jwts.builder()
+			.setHeaderParam(Header.TYPE, Header.JWT_TYPE) //Header 셋팅 : 토큰 타입 정보 typ
+			//payload - registered claim 셋팅
+			.setIssuer("team4")  //iss
+			.setIssuedAt(new Date(currentTime)) //iat
+			.setExpiration(new Date(now.getTime() + Duration.ofMillis(30).toMillis())) //exp
+			//payload - private claim
+			.claim("userId",userId)
+			.signWith(SignatureAlgorithm.HS256, "keySecret") //해싱알고리즘, 시크릿키
+			.compact();
+
+			return jwtToken;
 	}
 
 
