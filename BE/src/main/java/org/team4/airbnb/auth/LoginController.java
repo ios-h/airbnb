e9a6796462b446b6878f5aa517286cb54e77089b
mybloom <baejungeun11@gmail.com>
2022-06-10 11:03:01 +0900

    feat: 서버용client_id 적용 (ios-h/airbnb#35)

--- BE/src/main/java/org/team4/airbnb/auth/LoginController.java
+++ BE/src/main/java/org/team4/airbnb/auth/LoginController.java
@@ -9,7 +9,7 @@ public class LoginController {
 
 	@GetMapping("/login")
 	public RedirectView requestGithubLoginUrl(){
-		String githubLoginUrl = "https://github.com/login/oauth/authorize?client_id=76d1fc03a4cba8d91b42&scope=read:user";
+		String githubLoginUrl = "https://github.com/login/oauth/authorize?client_id=941c51754821fc586dd4&scope=read:user";
 		return new RedirectView(githubLoginUrl);
 	}
 }
