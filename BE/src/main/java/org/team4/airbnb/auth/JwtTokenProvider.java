e09d60a56985bf2d7547719973cee97c1c22af25
mybloom <baejungeun11@gmail.com>
2022-06-11 23:19:25 +0900

    fix: payload에 public claim 포함되게 하고, key 생성로직 변경 (ios-h/airbnb#35)
    - 시그니처 생성시 HS256 사용하도록 셋팅

--- BE/src/main/java/org/team4/airbnb/auth/JwtTokenProvider.java
+++ BE/src/main/java/org/team4/airbnb/auth/JwtTokenProvider.java
@@ -4,15 +4,16 @@ package org.team4.airbnb.auth;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.io.Encoders;
+import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import javax.crypto.SecretKey;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 import org.team4.airbnb.auth.domain.JwtPayload;
-import org.team4.airbnb.exception.TokenInValidateException;
 
+@Slf4j
 @PropertySource(value = "classpath:jwt.properties", ignoreResourceNotFound = true)
 @Component
 public class JwtTokenProvider {
@@ -20,66 +21,22 @@ public class JwtTokenProvider {
 	@Value("${jwt.secretKey}")
 	private String secretKey;
 
-
 	public String createToken(JwtPayload payload) {
-		SecretKey key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
-		Encoders.BASE64.encode(key.getEncoded());
+		SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
+
+		payload.getPrivateClaim().put(payload.getPublicClaim(), "true");
 		Claims claims = Jwts.claims(payload.getPrivateClaim());
 
 		String token = Jwts.builder()
+			.setHeaderParam("typ", "JWT")
 			.setSubject(payload.getSubject())
 			.setIssuedAt(payload.getIssuedAt())
 			.setExpiration(payload.getExpiration())
 			.setClaims(claims)
-			.signWith(key)
+			.signWith(key, SignatureAlgorithm.HS256)
 			.compact();
 
 		return token;
 	}
 
-//	public Claims parseJwtToken(String token) {
-//		String encodeSecretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
-//
-//		Claims claims = Jwts.parser()
-//			.setSigningKey(encodeSecretKey)
-//			.parseClaimsJws(token)
-//			.getBody();
-//
-//		return claims;
-//	}
-
-	public String getAccessTokenFromHeader(String header) {
-		validateHeader(header);
-		String token = extractToken(header);
-
-		return token;
-	}
-
-	private void validateHeader(String header) {
-		if (header == null || !header.startsWith("Bearer ")) {
-			throw new TokenInValidateException();
-		}
-	}
-
-	private String extractToken(String authorizationHeader) {
-		return authorizationHeader.substring("Bearer ".length());
-	}
-
-//	public void validateJwtToken(String accessToken) {
-//		String encodeSecretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
-//		Jws<Claims> claimsJws = Jwts.parser()
-//			.setSigningKey(encodeSecretKey)
-//			.parseClaimsJws(accessToken);
-//
-//		Claims body = claimsJws.getBody();
-//		JwsHeader jwsHeader = claimsJws.getHeader();
-//		String jwsSignature = claimsJws.getSignature();
-//
-//		JwsHeader header = claimsJws.getHeader();
-//		String signature = claimsJws.getSignature();
-//
-//		System.out.println(body);
-//		body.getExpiration();
-//		body.getIssuedAt();
-//	}
 }
