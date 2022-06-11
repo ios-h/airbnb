7d17c724947bae6b23608776e73e0c40d953126c
sanghee <anshee97@naver.com>
2022-06-11 09:52:41 +0900

    chore: WishListModel 수정 (ios-h/airbnb#41)

--- iOS/airbnb/Present/WishList/Model/WishListModel.swift
+++ iOS/airbnb/Present/WishList/Model/WishListModel.swift
@@ -9,7 +9,9 @@ import Foundation
 import UIKit.UIImage
 
 struct WishListModel {
-    let image: UIImage?
+    let id: Int
+    private(set) var image: UIImage? = UIImage(named: "img_gosim")
+    private(set) var imageUrls: [String]? = ["https://i.imgur.com/jujj5QL.jpg"]
     let rating: Double?
     let reviewCount: Int?
     let accomodationName: String
