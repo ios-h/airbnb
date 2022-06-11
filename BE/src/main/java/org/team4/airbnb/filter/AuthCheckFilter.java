bf9a637a49d42517dfe0110894222a8e50b0489f
mybloom <baejungeun11@gmail.com>
2022-06-12 00:58:53 +0900

    feat: 필터에 TokenExpiredException 익셉션 처리하도록 작성 (ios-h/airbnb#35)

--- BE/src/main/java/org/team4/airbnb/filter/AuthCheckFilter.java
+++ BE/src/main/java/org/team4/airbnb/filter/AuthCheckFilter.java
@@ -2,6 +2,7 @@ package org.team4.airbnb.filter;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Claims;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import javax.servlet.FilterChain;
@@ -16,6 +17,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.team4.airbnb.auth.JwtTokenProvider;
 import org.team4.airbnb.exception.ErrorResponse;
+import org.team4.airbnb.exception.TokenExpiredException;
 import org.team4.airbnb.exception.TokenInValidateException;
 
 @Slf4j
@@ -34,12 +36,17 @@ public class AuthCheckFilter extends OncePerRequestFilter {
 			validateHeader(header);
 			String accessToken = header.substring("Bearer ".length());
 
-			jwtTokenProvider.validateJwtToken(accessToken);
-//		Claims claims = jwtTokenProvider.parseJwtToken(accessToken);
+			Claims claims = jwtTokenProvider.validateJwtToken(accessToken);
 
 			filterChain.doFilter(request, response);
-		} catch (TokenInValidateException e) {
-			ErrorResponse errorResponse = new ErrorResponse(new String("invalid token".getBytes(),StandardCharsets.UTF_8));
+		} catch (TokenInValidateException inValidateException) {
+			ErrorResponse errorResponse = new ErrorResponse(
+				new String("invalid token".getBytes(), StandardCharsets.UTF_8));
+			response.setStatus(HttpStatus.UNAUTHORIZED.value());
+			response.getWriter().write(convertObjectToJson(errorResponse));
+		} catch (TokenExpiredException expiredException) {
+			ErrorResponse errorResponse = new ErrorResponse(
+				new String("authentication is expired".getBytes(), StandardCharsets.UTF_8));
 			response.setStatus(HttpStatus.UNAUTHORIZED.value());
 			response.getWriter().write(convertObjectToJson(errorResponse));
 		}
