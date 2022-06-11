b79f37b2a2b79b3ea6cecf1212fa797b1cce7d34
mybloom <baejungeun11@gmail.com>
2022-06-11 16:57:34 +0900

    fix:  클레임 생성 스펙대로 Map의 value 자료형을 String 에서 Object로 변경  (ios-h/airbnb#35)
    - Claims claims = Jwts.claims(payload.getPrivateClaim())

--- BE/src/main/java/org/team4/airbnb/auth/domain/JwtPayload.java
+++ BE/src/main/java/org/team4/airbnb/auth/domain/JwtPayload.java
@@ -4,7 +4,9 @@ import java.util.Date;
 import java.util.Map;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
 @Builder
 @AllArgsConstructor
 public class JwtPayload {
@@ -18,9 +20,9 @@ public class JwtPayload {
 	private final String publicClaim = "http://org.team4.airbnb";
 
 	//private claim
-	private final Map<String, String> privateClaim;
+	private final Map<String, Object> privateClaim;
 
-	public static JwtPayload of(Date issuedAt, Date expiration,	Map<String, String> privateClaim){
+	public static JwtPayload of(Date issuedAt, Date expiration,	Map<String, Object> privateClaim){
 
 		return JwtPayload.builder()
 			.issuedAt(issuedAt)
